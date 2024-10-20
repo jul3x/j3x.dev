238209cc69dd27cd331ddc844451ce43627cbe9a
jul3x <jprolejko@gmail.com>
2024-10-20 16:32:44 +0200

    Create blog and first blog post

---
+++ assets/js/no-widows.js
@@ -0,0 +1,22 @@
+const nbspSelectors = [
+	'h1',
+	'h2',
+	'h3',
+	'h4',
+	'h5',
+	'h6',
+	'p',
+	'blockquote'
+];
+
+const nbspWordMin = 6;
+
+nbspSelectors.forEach(function(selector) {
+	var nbspElements = document.querySelectorAll(selector);
+	nbspElements.forEach(function(element) {
+		var wordCount = (element.innerHTML.split(" ").length);
+		if (wordCount >= nbspWordMin) {
+			element.innerHTML = element.innerHTML.replace(/ ([^ ]*)$/,'&nbsp;$1');
+		}
+	});
+});
